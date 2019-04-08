461cfb70a8ffe9770b583da91385d850aa89e1b7
myrtokonst <myrtokonst@gmail.com>
2019-04-08 17:56:20 +0100

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.create(coupon_code:"eujhfn", store:"Ranjit's Rachet Rattattouille")
+Coupon.create(coupon_code:"hjdfgvsp", store:"Oliver's Lovely Olives")
+Coupon.create(coupon_code:"udskfho", store:"Pete's Petty Pranks")
